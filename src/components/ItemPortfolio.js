c113e96839e081dbe81398bc64c2a258a72b4949
Davi Bressiani <davi.bressiani@fatec.sp.gov.br>
2021-03-04 23:14:31 -0300

    add gh-pages

--- src/components/ItemPortfolio.js
+++ src/components/ItemPortfolio.js
@@ -4,11 +4,15 @@ const ItemPortfolio = ({ conteudo }) => {
   const { image, description, link } = conteudo;
   return (
     <div className="col-sm-4">
-      <div className="thumbnail imgPortfolio">
-        <img src={image} alt="teste" width="200" height="100" />
-        <p>{description}</p>
-        <a href={link}>Visitar</a>
-      </div>
+      <a href={link}>
+        <div className="thumbnail imgPortfolio">
+          <img src={image} alt="teste" width="200" height="100" />
+          <p>{description}</p>
+          <button className="text-center">
+            <a href={link}>Visitar</a>
+          </button>
+        </div>
+      </a>
     </div>
   );
 };
